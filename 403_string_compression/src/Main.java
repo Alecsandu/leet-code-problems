5f9f5236a431c5c380a567e009dd9d2342c5f3b2
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2024-12-27 14:39:13 +0200

    Resolve LC problem 403 String Compression
    Parse each character and start counting when there are duplicates

---
+++ 403_string_compression/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] input1 = new char[]{'a','a','b','b','c','c','c'};
+        System.out.println("Input1 res : " + compress(input1));
+    }
+
+    // https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
+    public static int compress(char[] chars) {
+        int newCharsPos = 0;
+        int n = chars.length;
+
+        for (int i = 0; i < n; i++) {
+            char ch = chars[i];
+            int count = 0;
+            while (i < n && ch == chars[i]) {
+                count++;
+                i++;
+            }
+
+            chars[newCharsPos++] = ch;
+            if (count > 1) {
+                for (char numCh: Integer.valueOf(count).toString().toCharArray()) {
+                    chars[newCharsPos++] = numCh;
+                }
+            }
+            i--;
+        }
+
+        return newCharsPos;
+    }
+}
