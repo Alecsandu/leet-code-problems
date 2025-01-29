1d3a3fd67f57359c39cab3e6e8548d7064ed5411
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-01-29 23:47:11 +0200

    Resolve LC problem 200 Number of Islands
    Traverse the grid and for each position do a BFS on neighbours of the current position and mark nearby islands as visited, for each new unvisited island increase their number.

---
+++ 200_number_of_islands/src/NumOfIslandsSimple.java
@@ -0,0 +1,67 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class NumOfIslandsSimple {
+
+    /**
+     * Puzzle description at <a href="https://leetcode.com/problems/number-of-islands/">link</a>.
+     */
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+
+        System.out.println("Number of islands is: " + Solver.numIslands(grid) + "(expected value is 1).");
+    }
+
+    static private class Solver {
+
+        private Solver() {
+            // no constructor
+        }
+
+        static int numIslands(char[][] grid) {
+            int numOfIslands = 0, rows = grid.length, cols = grid[0].length;
+            int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            Set<String> visited = new HashSet<>();
+
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    if (grid[row][col] == '1' && !visited.contains(row + "," + col)) {
+                        bfs(grid, row, col, visited, rows, cols, directions);
+                        numOfIslands++;
+                    }
+                }
+            }
+
+            return numOfIslands;
+        }
+
+        static void bfs(char[][] grid, int r, int c, Set<String> visited, int rows, int cols, int[][] directions) {
+            Queue<int[]> q = new LinkedList<>();
+            q.add(new int[]{r, c});
+            visited.add(r + "," + c);
+
+            while (!q.isEmpty()) {
+                int[] currentPos = q.poll();
+
+                for (int[] direction : directions) {
+                    int nextRow = direction[0] + currentPos[0], nextCol = direction[1] + currentPos[1];
+                    String nextPos = nextRow + "," + nextCol;
+                    if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && grid[nextRow][nextCol] == '1' && !visited.contains(nextPos)) {
+                        visited.add(nextPos);
+                        q.add(new int[]{nextRow, nextCol});
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
