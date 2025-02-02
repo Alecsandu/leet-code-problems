998b8e6151fe1fd9bcec6b53f6b03d573d0b61fd
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-02-02 18:42:41 +0200

    Resolve LC problem 643 Maximum Average Subarray I
    Find the maxSum of contiguous subarray of length k then compute the average.

---
+++ 643_maximum_average_subarray_1/src/MaximumAverageSubarray1.java
@@ -0,0 +1,64 @@
+/**
+ * Problem description at
+ * <a href="https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75">link</a>
+ */
+public class MaximumAverageSubarray1 {
+    public static void main(String[] args) {
+        Solution solution1 = new Solution1();
+        Solution solution2 = new Solution2();
+        int[] nums = new int[]{1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println("Solution 1 result is: " + solution1.findMaxAverage(nums, k) + "(should be 12.75)");
+        System.out.println("Solution 2 result is: " + solution2.findMaxAverage(nums, k) + "(should be 12.75)");
+
+        nums = new int[]{-1};
+        k = 1;
+        System.out.println("Solution 1 result is: " + solution1.findMaxAverage(nums, k) + "(should be -1.0)");
+        System.out.println("Solution 2 result is: " + solution2.findMaxAverage(nums, k) + "(should be -1.0)");
+    }
+
+    /**
+     * Find the max sum of contiguous subarray of length k, then calculate the average of the max.
+     */
+    static class Solution1 implements Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            int maxSum = Integer.MIN_VALUE;
+            for (int i = 0; i < nums.length - k + 1; i++) {
+                int currentSum = SolutionUtility.sumK(nums, i, i + k);
+                maxSum = Math.max(maxSum, currentSum);
+            }
+
+            return ((double) maxSum)/k;
+        }
+    }
+
+    /**
+     * Optimized the first solution.
+     * Use sum of first K numbers then just subtract the first number and add the next number.
+     */
+    static class Solution2 implements Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            int maxSum = SolutionUtility.sumK(nums, 0, k);
+            for (int i = k; i < nums.length; i++) {
+                int currentSum = maxSum - nums[i - k] + nums[i];
+                maxSum = Math.max(currentSum, maxSum);
+            }
+
+            return ((double) maxSum)/k;
+        }
+    }
+
+    static class SolutionUtility {
+        static int sumK(int[] nums, int start, int end) {
+            int sum = 0;
+            for (int i = start; i < end; i++) {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+
+    interface Solution {
+        double findMaxAverage(int[] nums, int k);
+    }
+}
