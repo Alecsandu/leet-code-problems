b4ec3129e5de2471b36cdbac4fdbe4902958f830
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-06-01 21:52:00 +0300

    Resolve LC problem 1768 MergeStringsAlternately smart solution
    Use an array to store the chars for new word and return the String at the final using the char array.

---
+++ 1768_merge_strings_alternately/src/MergeStringsAlternately.java
@@ -0,0 +1,50 @@
+public class MergeStringsAlternately {
+
+    public static void main(String[] args) {
+        Solver solver = new Solver();
+        System.out.println("Expected is : apbqcrds");
+        System.out.println("Actual is : " + solver.mergeAlternately("abcd", "pqrs"));
+    }
+
+    // https://leetcode.com/problems/merge-strings-alternately/description/
+    private static class Solver {
+        public String mergeAlternately(String word1, String word2) {
+            int n = word1.length(), m = word2.length();
+            int i = 0, j = 0, pos = 0;
+            Order order = Order.FIRST_WORD;
+
+            char[] newWord = new char[n + m];
+            while(i < n && j < m) {
+                if (order == Order.FIRST_WORD) {
+                    newWord[pos] = word1.charAt(i);
+                    i++;
+                    order = Order.SECOND_WORD;
+                } else {
+                    newWord[pos] = word2.charAt(j);
+                    j++;
+                    order = Order.FIRST_WORD;
+                }
+                pos++;
+            }
+
+            while (i < n) {
+                newWord[pos] = word1.charAt(i);
+                i++;
+                pos++;
+            }
+
+            while (j < m) {
+                newWord[pos] = word2.charAt(j);
+                j++;
+                pos++;
+            }
+
+            return new String(newWord);
+        }
+
+        private enum Order {
+            FIRST_WORD, SECOND_WORD
+        }
+    }
+
+}
