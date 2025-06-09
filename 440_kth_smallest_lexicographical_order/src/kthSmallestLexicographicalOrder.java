c87b58c9e26babcad6d6fabaeff92a6e3324b5f2
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-06-09 22:33:28 +0300

    Resolve LC problem 440 Kth smallest lexicographical order

---
+++ 440_kth_smallest_lexicographical_order/src/kthSmallestLexicographicalOrder.java
@@ -0,0 +1,41 @@
+public class kthSmallestLexicographicalOrder {
+
+    public static void main(String[] args) {
+        Solver solver = new Solver();
+        System.out.println("Actual: " + solver.findKthNumber(10, 3));
+        System.out.println("Expected: 10");
+    }
+
+    private static class Solver {
+
+        public int findKthNumber(int n, long k) {
+            int current = 1;
+            k--;
+
+            while (k > 0) {
+                long steps = countSteps(n, current, current + 1);
+                if (steps <= k) {
+                    current++;//move to the next prefix
+                    k -= steps;//skip numbers in the current prefix subtree
+                } else {
+                    current *= 10;
+                    k--;
+                }
+            }
+
+            return current;
+        }
+
+        private long countSteps(long n, long curr, long next) {
+            long steps = 0;
+            while (curr <= n) {
+                steps += Math.min(n + 1, next) - curr;
+                curr *= 10;
+                next *= 10;
+            }
+            return steps;
+        }
+
+    }
+
+}
